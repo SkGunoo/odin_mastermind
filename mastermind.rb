384b9b7fd29667b3fedae2bc15ffee613c09d796
Gunoo Kim <73500662+SkGunoo@users.noreply.github.com>
2025-04-23 21:18:56 +0100

    feat: add methods to display game board

--- mastermind.rb
+++ mastermind.rb
@@ -36,11 +36,20 @@
     #how am i gonna write the code for this? idk..  
 ## rubocop: enable all
 
+require 'colorize'
 
 
 module MasterMind
   
   class Game 
+    @@colours = ["red","blue","yellow","green","magenta","white","black","light_cyan"]
+    @@circle = "●"
+    @@empty_circle = "○"
+    @@coloured_symbols = @@colours.map {|element| @@circle.colorize(element.to_sym)} 
+    @@hint_symbols = [@@empty_circle,@@circle, @@circle.colorize(:red)]
+
+    attr_reader :board, :hint, :turns
+
     def initialize()
       puts "welcome to mastermind, please choose the game mode"
       @game_mode = ask_game_mode
@@ -49,6 +58,7 @@ module MasterMind
       @hint = Array.new(@turns)  { Array.new(4)}
     end
 
+    #make your select between game mode
     def ask_game_mode 
       available_modes = [1,2]
       answer = nil
@@ -59,6 +69,7 @@ module MasterMind
       answer 
     end
 
+    #how many turns the user want? 
     def ask_for_number_of_turns
       turn_range = (12..20).to_a
       answer = nil
@@ -68,6 +79,38 @@ module MasterMind
       end 
       answer
     end
+
+    #draws game board
+    def draw_board_and_hints
+      ##      {o|o|o|o}  hint: [|o|o|o|o|] 
+      #some buffer for better terminal viewing experience
+      3.times {puts ""}
+      ##combine each row from two arrays as a element of new array
+      completed_game_board = turns.times.map {|row|"     { #{get_one_row(row,board,@@coloured_symbols)} }    hint: [ #{get_one_row(row,hint,@@hint_symbols)} ]"}
+      ##so, i can add the visual seperaters with code below
+      puts completed_game_board.join("\n---------------------------------------\n")     
+      3.times {puts ""}
+
+    end
+
+    ##turn the number values from @board and @hint and turn them into array of symbols
+    def get_one_row(row_number,board_or_hint, symbol_list)
+      row = board_or_hint[row_number].map do |element|
+        if element == nil 
+          element = @@empty_circle
+        else
+          element = symbol_list[element]
+        end  
+      end
+      row.join("|")
+    end
+
+    ##method to put random numbers on the board 
+    def random_num_gen
+      board.each do |row|
+        row.each_with_index {|element,index| row[index] = Random.rand(0..7)}
+      end
+    end
   end
 end
 
@@ -75,3 +118,9 @@ end
 include MasterMind
 
 new_game = Game.new() 
+
+new_game.random_num_gen
+
+new_game.draw_board_and_hints
+
+# puts new_game.board
