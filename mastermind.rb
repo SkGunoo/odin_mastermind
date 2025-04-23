1cf76e69a1a0f0d61a5bd3e0aad803b50df56596
Gunoo Kim <73500662+SkGunoo@users.noreply.github.com>
2025-04-23 17:03:04 +0100

    feat: ask user about game modes & turns

--- mastermind.rb
+++ mastermind.rb
@@ -35,3 +35,43 @@
   #methods to choose the colours with ai
     #how am i gonna write the code for this? idk..  
 ## rubocop: enable all
+
+
+
+module MasterMind
+  
+  class Game 
+    def initialize()
+      puts "welcome to mastermind, please choose the game mode"
+      @game_mode = ask_game_mode
+      @turns = ask_for_number_of_turns 
+      @board = Array.new(@turns) { Array.new(4)}
+      @hint = Array.new(@turns)  { Array.new(4)}
+    end
+
+    def ask_game_mode 
+      available_modes = [1,2]
+      answer = nil
+      until available_modes.include?(answer)
+        puts "choose game mode, for humanplayer: 1 , for computerplayer: 2"
+        answer = gets.chomp.to_i 
+      end 
+      answer 
+    end
+
+    def ask_for_number_of_turns
+      turn_range = (12..20).to_a
+      answer = nil
+      until turn_range.include?(answer)
+        puts "choose number of turns, between 12 - 20"
+        answer = gets.chomp.to_i 
+      end 
+      answer
+    end
+  end
+end
+
+
+include MasterMind
+
+new_game = Game.new() 
